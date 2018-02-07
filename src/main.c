fec084d9bf53b973cf04f9bd7665dd4d179f8ebd
Tom McLeod <tom.mcleod@opalkelly.com>
2018-02-06 17:47:04 -0800

    Add test pod firmware

--- src/main.c
+++ src/main.c
@@ -35,41 +35,89 @@
 #include "syzygy_seq.h"
 #include "syzygy_dna_fw.h"
 
-// The main loop executes as follows:
-// - Initialize ADC and power sequencer timer hardware
-// - Initialize the power sequencer enable outputs
-// - Read the analog voltage on the R_GA pin
-// - Convert the R_GA voltage to an I2C address
-// - Initialize the I2C handler with the I2C address
-// - Initialize the dna_fw timer used to handle I2C transactions
-// - Run through the power sequencing code
-// - Loop forever, doing nothing
+// Assume +/- 5% as per SYZYGY spec
+#define HIGH_THRESH_3v3 690 // 690 mV after resistor divider
+#define LOW_THRESH_3v3  624 // 624 mV after resistor divider
+// Assume +/- 10% as per SYZYGY spec
+#define HIGH_THRESH_5v  1096 // 1.096 V after resistor divider
+#define LOW_THRESH_5v   897  // 897 mV after resistor divider
+// Assume +/- 5% as per SYZYGY spec and a 1.8V VIO
+#define HIGH_THRESH_VIO 376 // 376 mV after resistor divider
+#define LOW_THRESH_VIO  340 // 340 mV after resistor divider
+
+// TODO: Don't do this, use 1.8 instead due to bank compatibility
+// Assume +/- 5% as per SYZYGY spec and a 3.3V VIO
+//#define HIGH_THRESH_VIO 690 // 376 mV after resistor divider
+//#define LOW_THRESH_VIO  624 // 340 mV after resistor divider
+
+// Configure the pins used by test pods
+void config_test_mode_pins()
+{
+	DDRB = 7;
+	DDRA &= ~(1 << DDA7);
+}
+
+// Perform test pod checks and communicate with FPGA
+// Communication is as follows:
+// If (TEST_MODE_0 high)
+//    TEST_MODE_1 = 3.3V_good
+//    TEST_MODE_2 = 5V_good
+//    TEST_MODE_3 = VIO_good
+// else
+//    TEST_MODE_1 = !3.3V_good
+//    TEST_MODE_2 = !5V_good
+//    TEST_MODE_3 = !VIO_good
+void test_pod_check()
+{
+	uint8_t status_field = 0;
+	uint32_t adc_reading;
+	
+	// Read 5V ADC and check
+	set_adc_mux(SQ0_ADC_MUX);
+	START_ADC;
+	adc_reading = (uint32_t) read_adc();
+	adc_reading = adc_reading * ADC_MV;
+	adc_reading = adc_reading >> ADC_BITS;
+	if ((adc_reading < HIGH_THRESH_5v) && (adc_reading > LOW_THRESH_5v)) {
+		status_field |= 0x1;
+	}
+	
+	// Read VIO ADC and check
+	set_adc_mux(SQ1_ADC_MUX);
+	START_ADC;
+	adc_reading = (uint32_t) read_adc();
+	adc_reading = adc_reading * ADC_MV;
+	adc_reading = adc_reading >> ADC_BITS;
+	if ((adc_reading < HIGH_THRESH_VIO) && (adc_reading > LOW_THRESH_VIO)) {
+		status_field |= 0x2;
+	}
+	
+	// Read 3.3V ADC and check
+	set_adc_mux(SQ2_ADC_MUX);
+	START_ADC;
+	adc_reading = (uint32_t) read_adc();
+	adc_reading = adc_reading * ADC_MV;
+	adc_reading = adc_reading >> ADC_BITS;
+	if ((adc_reading < HIGH_THRESH_3v3) && (adc_reading > LOW_THRESH_3v3)) {
+		status_field |= 0x4;
+	}
+	
+	// Check Test Mode 0 pin and return status
+	if (PINA & (1 << PINA7)) {
+		PORTB = status_field & 0x7;
+	} else {
+		PORTB = (~status_field) & 0x7;
+	}
+}
+
 int main(void)
 {
 	uint16_t adc_val = 0;
 	uint8_t i2c_addr = 0;
-	uint8_t i;
-	sequence_config current_seq_config;
-	 // Track the delay time for each enable channel
-	uint32_t channel_timer_count[3];
-	// The following variables are used to store bitfields in which
-	// each bit corresponds to a true (1) or false (0) value for each
-	// enable channel or adc input.
-	uint8_t channel_threshold_passed = 0;
-	uint8_t used_analog_inputs = 0;
-	uint8_t crossed_thresholds = 0;
-	uint8_t channel_delay_done = 0;
-	uint8_t used_enables = 0;
-	uint8_t channel_enable_done = 0;
+	
+	config_test_mode_pins();
 
 	init_adc();
-	
-	init_seq_timer();
-	
-	read_seq_config(&current_seq_config);
-	
-	// Set sequencer pins to output in the inactive state
-	init_seq_outputs(current_seq_config.en_config);
 	
 	// Get RGA ADC reading
 	adc_val = read_adc();
@@ -89,43 +137,11 @@ int main(void)
 	// Set global interrupt enable (required for I2C communication)
 	sei();
 	
-	// Populate the used_ bitfields based on the sequencer configuration
-	for(i = 0; i < 3; i++) {
-		used_analog_inputs |= current_seq_config.en_config[i] & 0x7;
-		used_enables |= ((current_seq_config.en_config[i] & (1 << EN_ENABLED_BIT)) ? 0 : 1) << i;
-	}
-	
-	// Power sequencing loop
-	while ((channel_enable_done & used_enables) != used_enables) {
-		// Update the necessary power supply readings for each analog input
-		for(i = 0; i < 3; i++) {
-			if ((used_analog_inputs & ~crossed_thresholds) & (1 << i)) {
-				crossed_thresholds |= test_threshold(i, current_seq_config.threshold[i]) << i;
-			}
-		}
-			
-		// Iterate over each sequencer enable channel and check if it's ready to output
-		for(i = 0; i < 3; i++) {
-			if (channel_delay_done & (1 << i)) {
-				// The delay is complete, activate sequencer enable
-				set_seq_enable(i, current_seq_config.en_config[i]);
-				channel_enable_done |= 1 << i;
-			} else if (channel_threshold_passed & (1 << i)) {
-				// All necessary thresholds have passed, check if the channel
-				// delay has completed
-				channel_delay_done |= check_delay(channel_timer_count[i], current_seq_config.delay[i]) << i;
-			} else if ((current_seq_config.en_config[i] & crossed_thresholds) == (current_seq_config.en_config[i] & 0x7)) {
-				// We've crossed all necessary thresholds for this channel,
-				// record the current timer count to allow for delay measurement
-				channel_timer_count[i] = current_seq_time_ms();
-				channel_threshold_passed |= 1 << i;
-			}
-		}
-	}
-	
 	// Main application loop (runs after power sequencing completes
-	while (1) {
-		
+	while (1) 
+	{
+		// Test pod specific functionality
+		test_pod_check();
 	}
 }
 
